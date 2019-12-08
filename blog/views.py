4734d2f105fb385d5d311c20b1c0b87f192172b6
Tylor Schafer <tylorschafer@gmail.com>
2019-12-07 19:11:53 -0700

    Add blog_detail and blog_category view functions

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,25 @@ def blog_index(request):
         'posts': posts,
     }
     return render(request, "blog_index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    return render(request, 'blog_category.html', context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        'post': post,
+        'comments': comments,
+    }
+
+    return render(request, 'blog_detail.html', context)

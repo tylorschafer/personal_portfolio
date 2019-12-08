0e66e33cb2beda6f712bbc34eb270d73cd56ff3b
Tylor Schafer <tylorschafer@gmail.com>
2019-12-07 19:27:07 -0700

    Add Comment Form to blog_detail view function

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post
+from .forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
@@ -22,10 +23,23 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if from.is_valid():
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         'post': post,
         'comments': comments,
+        'form': form,
     }
 
     return render(request, 'blog_detail.html', context)
